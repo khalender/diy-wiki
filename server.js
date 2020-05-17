80bec51cfdf094a47ac713eb8288c17cf64c0c6e
khalender <m_nuralp@gitlab.com>
2020-05-17 07:11:44 +0200

    page listing/reading works

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const util = require('util');
-const fs = require('fs');
+const {promises: fs} = require('fs');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 // Uncomment this out once you've made your first route.
-// app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 // some helper functions you can use
 const readFile = util.promisify(fs.readFile);
@@ -31,19 +31,32 @@ function jsonError(res, message) {
   res.json({ status: 'error', message });
 }
 
-app.get('/', (req, res) => {
-  res.json({ wow: 'it works!' });
-});
+// app.get('/', (req, res) => {
+//   res.json({ wow: 'it works!' });
+// });
 
 // If you want to see the wiki client, run npm install && npm build in the client folder,
 // then comment the line above and uncomment out the lines below and comment the line above.
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-// });
+app.get('/wiki/**', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 // GET: '/api/page/:slug'
 // success response: {status: 'ok', body: '<file contents>'}
 // failure response: {status: 'error', message: 'Page does not exist.'}
+app.get('/api/page/:slug', async (req,res)=> {
+  console.log(req.params);
+  try{
+ const fileContent = await fs.readFile(DATA_DIR +"/"+ req.params.slug + ".md","utf8");
+} catch(e){
+  //terrible work around but it eventually works :)
+    fileContent =  await fs.readFile(DATA_DIR +"/"+ req.params.slug,"utf8");
+  }
+
+ console.log(fileContent);
+  jsonOK(res,{"body": fileContent});
+});
+
 
 // POST: '/api/page/:slug'
 // body: {body: '<file text content>'}
@@ -60,15 +73,18 @@ app.get('/', (req, res) => {
 //  tags are any word in all documents with a # in front of it
 // failure response: no failure response
 
+
+
 // GET: '/api/tags/:tag'
 // success response: {status:'ok', tag: 'tagName', pages: ['tagName', 'otherTagName']}
 //  file names do not have .md, just the name!
 // failure response: no failure response
 
-app.get('/api/page/all', async (req, res) => {
+app.get('/api/pages/all', async (req, res) => {
   const names = await fs.readdir(DATA_DIR);
+    
   console.log(names);
-  jsonOK(res, { });
+  jsonOK(res, {"pages": names});
 });
 
 const port = process.env.PORT || 5000;
